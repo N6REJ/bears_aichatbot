08aabb88c32278ec337605292c3bd7ee3e4e0e33
Bear <programming@hallhome.us>
2025-09-12 05:44:24 -0500

    Update version to 2025.09.12.40 and override display() method in DisplayController classes to force correct Joomla 5 admin view prefix and model attachment

--- com_bears_aichatbot/src/Controller/DisplayController.php
+++ com_bears_aichatbot/src/Controller/DisplayController.php
@@ -8,9 +8,36 @@ namespace Joomla\Component\BearsAichatbot\Controller;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController as JBaseController;
 
 class DisplayController extends JBaseController
 {
     protected $default_view = 'dashboard';
+
+    public function display($cachable = false, $urlparams = [])
+    {
+        $app   = Factory::getApplication();
+        $input = $app->input;
+        $viewName = $input->getCmd('view', $this->default_view ?: 'dashboard');
+        $input->set('view', $viewName);
+
+        // Force the correct Joomla 5 Administrator view prefix
+        $prefix = 'Joomla\\Component\\BearsAichatbot\\Administrator\\View\\';
+
+        // Create the view and attach the corresponding model if present
+        $view = $this->getView(ucfirst($viewName), 'html', $prefix);
+        try {
+            $modelClass = ucfirst($viewName);
+            $model = $this->getModel($modelClass);
+            if ($model) {
+                $view->setModel($model, true);
+            }
+        } catch (\Throwable $ignore) {}
+
+        $view->document = Factory::getDocument();
+        $view->display();
+
+        return $this;
+    }
 }
