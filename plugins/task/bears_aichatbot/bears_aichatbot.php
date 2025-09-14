ee10a578e77fc7194771e1a014502e6e40462d04
Bear <programming@hallhome.us>
2025-09-14 13:02:37 -0500

    Fix PHP entry file formatting and clean up corrupted content

--- plugins/task/bears_aichatbot/bears_aichatbot.php
+++ plugins/task/bears_aichatbot/bears_aichatbot.php
@@ -1,1 +1,5 @@
-<?php\n/**\n * Entry file for plg_task_bears_aichatbot. Bootstrap handled via services/provider and autoload.\n */\n\defined('_JEXEC') or die  Move-Item -Force 'plugins/task/bears_aichatbot/src/Exte
+<?php
+/**
+ * Entry file for plg_task_bears_aichatbot. Bootstrap handled via services/provider and autoload.
+ */
+\defined('_JEXEC') or die;
